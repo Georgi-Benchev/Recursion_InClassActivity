79571e067d47a06d52dc1b80e55564c0dba874a7
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2024-12-06 16:27:33 +0200

    implementing method traverseDirectories && method traverse in FilesUtils class

--- src/com/company/dsa/FilesUtils.java
+++ src/com/company/dsa/FilesUtils.java
@@ -1,11 +1,33 @@
 package com.company.dsa;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
 public class FilesUtils {
     public static void traverseDirectories(String path) {
-        throw new UnsupportedOperationException();
+        File directory = new File(path);
+
+        if (directory.isDirectory()) {
+            System.out.println(directory.getName() + ":");
+            traverse(directory, 1);
+        }
+    }
+
+    private static void traverse(File file, int indentLevel) {
+
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (File fileInArray : files) {
+                String spacings = "  ".repeat(indentLevel);
+                if (fileInArray.isDirectory()) {
+                    System.out.println(spacings + fileInArray.getName() + ":");
+                    traverse(fileInArray, indentLevel + 1);
+                } else {
+                    System.out.println(spacings + fileInArray.getName());
+                }
+            }
+        }
     }
 
     public static List<String> findFiles(String path, String extension) {
